5200057d23a8f95239c3833f4888554b639a759a
huytd1012 <82645696+huytd1012@users.noreply.github.com>
2022-02-28 22:43:51 +0700

    Create B5W2.cpp

---
+++ week2/B5W2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for( int i = 1; i <= 2*n+1; ++i ){
+        for( int j = 1; j <= 2*n+1; ++j )
+            if( i == j || i + j - 1 == 2*n + 1 ) cout << "* ";
+            else cout << ". ";
+        cout << '\n';
+    }
+}
