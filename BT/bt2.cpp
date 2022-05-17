4925bbaeeaf55fe8aaf08dc014fe0237fbeb8aed
huytd1012 <82645696+huytd1012@users.noreply.github.com>
2022-05-17 12:45:46 +0700

    Create bt2.cpp

---
+++ BT/bt2.cpp
@@ -0,0 +1,12 @@
+/*Viết chương trình đếm số lượng tên trong một xâu ký tự. Tên hợp lệ là một từ nhiều hơn hoặc bằng 2 ký tự có ký tự đầu tiên là một chữ in hoa.
+
+Đầu vào: Một dòng duy nhất chứa đường dẫn đến một file đầu vào.
+
+Nội dung của file như sau:
+
+Dòng đầu tiên là một số nguyên N.
+N dòng tiếp theo mỗi dòng chứa một xâu ký tự gồm nhiều từ, cách nhau bởi dấu cách.
+Đầu ra: N dòng tương ứng là các số lượng tên của đầu vào.
+
+Gợi ý: Ký tự đầu tiên của một từ là ký tự đầu xâu hoặc trước nó có một dấu cách.
+*/
